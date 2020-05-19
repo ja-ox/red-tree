3583daf156967207190815a3cb24d49f3db86f9c
Jacob <47821203+ja-ox@users.noreply.github.com>
2020-05-19 17:30:12 +0100

    Update similarities.py

--- similarities.py
+++ similarities.py
@@ -12,23 +12,30 @@ import networkx as nx
 import pandas as pd
 import numpy as np
 from scipy.spatial.distance import euclidean, pdist, squareform
-
-# # Load file
-# file = ''
-# df = pd.read_csv(file)
-
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
 
 n = 1000 # Number of patients
 m = 10 # Number of diseases
 
-# Random array of morbidities per patient
-data = [np.random.choice([0, 1], size=(m,), p=[0.2, 0.8]) for patient in range(n)]
-cols = [f'41270.{i}' for i in range(m)]
-df = pd.DataFrame(data, columns = cols)
+# Simulated data - random array of morbidities per patient
+simulate = False
+
+if simulate:
+    data = [np.random.choice([0, 1], size=(m,), p=[0.2, 0.8]) for patient in range(n)]
+    cols = [f'41270.{i}' for i in range(m)]
+    df = pd.DataFrame(data, columns = cols)
+else:
+    ## Load file
+    file = r'C:\Users\Jacob\Downloads\autoencoders\data\ukb_latest-ICD10-onehot.tsv'
+    df = pd.read_csv(file, sep='\t', nrows=1000)
+    cols = df.columns
+    
 
 # Select only ICD-10 columns
 mask = df.columns.str.startswith('41270')
 df = df[df.columns[mask]]
+cols = cols[mask]
 
 # Transpose dataframe for easier computing of similarity etc
 df = df.T
@@ -44,6 +51,15 @@ def similarity_func(u, v):
 similarities = pdist(df, similarity_func) # Similarity measure
 dists = pdist(df) # Distance measure
 df_euclid = pd.DataFrame(squareform(dists), columns=df.index, index=df.index)
+
+# Visualise similarities
+fig, ax = plt.subplots(dpi=500)
+fig.set_size_inches(20,17)
+sns.heatmap(df_euclid.head(n=5000).iloc[:,:5000], square=True, figure=fig)
+plt.show()
+
+#plt.imshow(df_euclid.values, interpolation="nearest", cmap='Blues')
+#plt.show()
 
 # Generate clusters
 
