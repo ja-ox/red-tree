782365d088ce51d47ac69d57c942630502296a7c
Jacob <47821203+ja-ox@users.noreply.github.com>
2020-05-27 18:27:37 +0100

    Add similarity measure.

--- similarities.py
+++ similarities.py
@@ -15,7 +15,7 @@ from scipy.spatial.distance import euclidean, pdist, squareform
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
 
-n = 1000 # Number of patients
+n = 500 # Number of patients
 m = 10 # Number of diseases
 
 # Simulated data - random array of morbidities per patient
@@ -28,7 +28,7 @@ if simulate:
 else:
     ## Load file
     file = r'C:\Users\Jacob\Downloads\autoencoders\data\ukb_latest-ICD10-onehot.tsv'
-    df = pd.read_csv(file, sep='\t', nrows=100)
+    df = pd.read_csv(file, sep='\t', nrows=n)
     cols = df.columns
     
 
@@ -36,9 +36,11 @@ else:
 mask = df.columns.str.startswith('41270')
 df = df[df.columns[mask]]
 
-#%%
+# Summary stats
+totals = df.sum(axis=1)
+avg = np.mean(totals)
 
-# Renaming columns
+# Renaming columns with ICD-10 codes
 col_dict_file = r'C:\Users\Jacob\Downloads\autoencoders\data\ukb_latest-ICD10-onehot_icd10_map.txt'
 df_dict = pd.read_csv(col_dict_file, sep='\t')
 df_dict = df_dict[['value', 'code']]
@@ -47,35 +49,58 @@ code_dict = df_dict.set_index('value').T.to_dict('records')[0]
 df.columns = [code_dict[int(val.replace('41270-0.',''))] for val in df.columns]
 cols = df.columns
 
-#%%
-
-# Transpose dataframe for easier computing of similarity etc
+# Transpose dataframe so that each row is a disease
 df = df.T
 df.index = cols
 
 ## MULTIMORBIDITIES
 
-# Generate Similarity Matrix
+# Generate Disease x Disease Similarity Matrix
 
-def similarity_func(u, v):
-    return 1/(1+euclidean(u,v))
+def check_symmetric(a, rtol=1e-05, atol=1e-08):
+    return np.allclose(a, a.T, rtol=rtol, atol=atol)
 
-similarities = pdist(df, similarity_func) # Similarity measure
-dists = pdist(df) # Distance measure
-df_euclid = pd.DataFrame(squareform(dists), columns=df.index, index=df.index)
+# https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.DistanceMetric.html
+# Metrics intended for boolean-valued vector spaces:
 
-n = len(cols)
+from sklearn.neighbors import DistanceMetric
+
+dist = DistanceMetric.get_metric('sokalsneath')
+dists = dist.pairwise(df)
+#df_dists = pd.DataFrame(dists, columns=df.index, index=df.index)
+
+m = len(cols)
+
+
+# DO I need to remove patients which have 0 for everything??? Is this messing up the metrics?
+
+# Want metric that cares when both diseases have TT, ignores NN, and weights by TN NT
+# i.e. TT / (TT+NT+TN) ? e.g. DICE and SOKALSNEATH both include TT in denominator (dist)
 
 # Visualise similarities
-fig, ax = plt.subplots(dpi=500)
+fig, ax = plt.subplots(dpi=250)
 fig.set_size_inches(20,17)
-sns.heatmap(df_euclid.head(n=n).iloc[:,:n], square=True, figure=fig)
+sns.heatmap(dists, square=True, figure=fig)
 plt.show()
 
 #plt.imshow(df_euclid.values, interpolation="nearest", cmap='Blues')
 #plt.show()
 
+if False:
+    plt.bar(list(range(0,len(totals.value_counts())))[:40], [100 * totals.value_counts() / m][0][:40])
+
+#%%
+
 # Generate edges
+
+up_tri = dists.copy()
+up_tri[np.tril_indices_from(up_tri)] = np.nan
+
+print(f'   1 : {len(np.argwhere(up_tri == 1))}')
+print(f'(0,1): {len(np.argwhere(up_tri < 1)) - len(np.argwhere(up_tri == 0))}')
+print(f'   0 : {len(np.argwhere(up_tri == 0))}')
+
+edges = [(cols[i], cols[j]) for i, j in np.argwhere(up_tri == 0)]
 
 # Create graph
 
